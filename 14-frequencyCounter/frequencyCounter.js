524afc1805b8a87149552b5f0a47728b698d693b
hady-bah <80234152+hady-bah@users.noreply.github.com>
2023-06-10 17:35:07 -0400

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,13 @@
 function frequencyCounter(word) {
   // Insert code here;
+  const counter = {};
+  
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i];
+    counter[char] = (counter[char] || 0) + 1;
+  }
+  
+  return counter;
 }
 
 // Do not edit this line;
