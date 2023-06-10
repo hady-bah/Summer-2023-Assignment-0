30e5231ff8c52e658108ff395ee7c23f24fe3247
hady-bah <80234152+hady-bah@users.noreply.github.com>
2023-06-10 16:24:54 -0400

    Update fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,26 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  const nums = [];
+  
+  
+  for(let i = start; i <= end; i++){
+    nums.push(i);
+  }
+  
+  for(let i = 0; i < nums.length; i++){
+   if(nums[i]%3 == 0 && nums[i]%5 === 0){
+     nums[i] = "FizzBuzz";
+   }
+   else if(nums[i]%3 === 0){
+     nums[i] = "Fizz";
+   }
+   else if(nums[i]%5 === 0){
+     nums[i] = "Buzz";
+   }
+    
+  }  
+    
+  return nums;
 }
 
 // Do not edit this line;
