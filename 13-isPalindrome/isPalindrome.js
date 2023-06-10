067595fa59b6c0e07ca53020965be4bb0fef09bf
hady-bah <80234152+hady-bah@users.noreply.github.com>
2023-06-10 16:36:25 -0400

    Update isPalindrome.js

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,8 @@
 function isPalindrome(word) {
   // Insert code here;
+  const reversedWord = word.split('').reverse().join('');
+  
+  return word === reversedWord; 
 }
 
 // Do not edit this line;
