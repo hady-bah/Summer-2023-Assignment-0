b9b913ece6fff0c13b30b0e9b483c52acca8859c
hady-bah <80234152+hady-bah@users.noreply.github.com>
2023-06-11 18:20:15 -0400

    Update pairSum.js

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,16 @@
 function pairSum(nums, target) {
   // Insert code here;
+  const numSet = new Set();
+  
+  for(let i = 0; i < nums.length; i++){
+    const complement = target - nums[i];
+    if (numSet.has(complement)){
+       return true;  
+     }
+   numSet.add(nums[i]);  
+  }
+  
+  return false; 
 }
 
 // Do not edit this line;
