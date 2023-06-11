81f9e8000bcf24639adef0afc62d046e29051ea5
hady-bah <80234152+hady-bah@users.noreply.github.com>
2023-06-11 17:58:19 -0400

    Update setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,14 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  const unionSet = new Set();
+  
+  for(const set of args){
+    for(const element of set){
+      unionSet.add(element);
+    }
+  }
+  return unionSet;
+  
 }
 
 // Do not edit this line;
