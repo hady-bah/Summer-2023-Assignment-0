b5d74e6da1f5cb2d0a552cd9103cd073f6cb24ea
hady-bah <80234152+hady-bah@users.noreply.github.com>
2023-06-11 17:34:53 -0400

    Update countDownSum.js

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,6 +1,14 @@
 class MySolution {
   countDownSum(num) {
     // Insert code here;
+    let count = num;
+    
+    
+    for(let i = 1; i <= num; i++){
+      count += num - i;
+    }
+    
+    return count;
   }
 }
 
