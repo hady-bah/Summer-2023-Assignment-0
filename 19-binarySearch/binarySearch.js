90a3fa4b02bc734d2997a20fc7b93515ef10ffb4
hady-bah <80234152+hady-bah@users.noreply.github.com>
2023-06-12 09:54:31 -0400

    Update binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,23 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    const search = (low, high) => {
+      if (low > high) {
+        return false;
+      }
+
+      const mid = Math.floor((low + high) / 2);
+
+      if (nums[mid] === target) {
+        return true;
+      } else if (nums[mid] > target) {
+        return search(low, mid - 1);
+      } else {
+        return search(mid + 1, high);
+      }
+    };
+
+    return search(0, nums.length - 1);
   }
 }
 
