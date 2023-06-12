8004205fe8ad478f6c3c0fd748a91272feb17378
hady-bah <80234152+hady-bah@users.noreply.github.com>
2023-06-11 23:05:34 -0400

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,15 @@
 function titleCaseEdit(title) {
   // Insert code here;
+ 
+  const words = title.toLowerCase().split(" ");
+  
+  const capitalizedWords = words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+
+  const titleCase = capitalizedWords.join(" ");
+
+  return titleCase;
 }
 
 // Do not edit this line;
